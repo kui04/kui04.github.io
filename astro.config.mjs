23211103261a513ca6a8a10275d211515c6b4398
kui04 <likuiandmc2004@gmail.com>
2026-04-22 15:19:47 +0800

    style: update expressive code themes and customize theme handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,31 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://kui04.github.io",
   prefetch: true,
   integrations: [
     expressiveCode({
-      themes: ["dark-plus"],
+      themes: ["one-light", "material-theme-darker"],
       styleOverrides: {
         codeFontFamily: "monospace",
       },
+      customizeTheme: (theme) => {
+        if (theme.name.includes("light")) theme.name = "light";
+        else if (theme.name.includes("dark")) theme.name = "dark";
+      },
+      themeCssSelector: (theme) => {
+        if (theme.name === "dark") return ".dark";
+        else return ":root";
+      },
+      useDarkModeMediaQuery: false,
     }),
     mdx(),
     sitemap(),
   ],
-
   vite: {
     plugins: [tailwindcss()],
   },
